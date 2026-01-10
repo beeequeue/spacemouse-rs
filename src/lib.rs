b0d6819ce96e0a4a95e77be428b8ee0e874c4ae7
bq <adam@haglund.dev>
2026-01-11 00:56:33 +0100

    start moving camera

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use godot::{
-    classes::{Control, EditorPlugin, IEditorPlugin, editor_plugin::DockSlot},
+    classes::{
+        Camera3D, Control, EditorPlugin, IEditorPlugin, InputEvent, Label, editor_plugin::DockSlot,
+    },
     global::print,
     prelude::*,
 };
@@ -46,7 +48,7 @@ const SPACE_MOUSE_HIDS: &[SpaceMouseDevice; 7] = &[
 struct SpaceMouse;
 
 impl SpaceMouse {
-    pub fn find_spacemouse(hidapi: &HidApi) -> Option<SpaceMouseDevice> {
+    pub fn find(hidapi: &HidApi) -> Option<SpaceMouseDevice> {
         for device in hidapi.device_list() {
             for spacemouse in SPACE_MOUSE_HIDS {
                 if device.vendor_id() == spacemouse.vid && device.product_id() == spacemouse.pid {
@@ -59,13 +61,17 @@ impl SpaceMouse {
     }
 }
 
+fn to_i16(slice: &[u8]) -> i16 {
+    i16::from_le_bytes(slice.try_into().unwrap())
+}
+
 fn read_data(spacemouse: SpaceMouseDevice, hid: &HidDevice) -> (Vector3, Vector3) {
     match spacemouse.format {
         Format::Original => {
             let mut translation = Vector3::ZERO;
             let mut rotation = Vector3::ZERO;
 
-            for _ in 0..1 {
+            for _ in 0..4 {
                 let buffer: &mut [u8; 7] = &mut [0; 7];
                 let result = hid.read(buffer);
                 if result.is_err() {
@@ -74,13 +80,13 @@ fn read_data(spacemouse: SpaceMouseDevice, hid: &HidDevice) -> (Vector3, Vector3
 
                 let first = *buffer.first().unwrap();
                 if first == 1 {
-                    translation.x = *buffer.get(1).unwrap() as f32;
-                    translation.y = *buffer.get(5).unwrap() as f32;
-                    translation.x = *buffer.get(3).unwrap() as f32;
+                    translation.x = to_i16(&buffer[1..=2]) as f32;
+                    translation.y = -to_i16(&buffer[5..=6]) as f32;
+                    translation.z = to_i16(&buffer[3..=4]) as f32;
                 } else if first == 2 {
-                    rotation.x = *buffer.get(1).unwrap() as f32;
-                    rotation.y = *buffer.get(5).unwrap() as f32;
-                    rotation.x = *buffer.get(3).unwrap() as f32;
+                    rotation.x = to_i16(&buffer[1..=2]) as f32;
+                    rotation.y = -to_i16(&buffer[5..=6]) as f32;
+                    rotation.z = to_i16(&buffer[3..=4]) as f32;
                 }
             }
 
@@ -99,12 +105,12 @@ fn read_data(spacemouse: SpaceMouseDevice, hid: &HidDevice) -> (Vector3, Vector3
 
             let first = *buffer.first().unwrap();
             if first == 1 {
-                translation.x = *buffer.get(1).unwrap() as f32;
-                translation.y = *buffer.get(5).unwrap() as f32;
-                translation.x = *buffer.get(3).unwrap() as f32;
-                rotation.x = *buffer.get(7).unwrap() as f32;
-                rotation.y = *buffer.get(11).unwrap() as f32;
-                rotation.x = *buffer.get(9).unwrap() as f32;
+                translation.x = to_i16(&buffer[1..=2]) as f32;
+                translation.y = -to_i16(&buffer[5..=6]) as f32;
+                translation.z = to_i16(&buffer[3..=4]) as f32;
+                rotation.x = to_i16(&buffer[7..=8]) as f32;
+                rotation.y = -to_i16(&buffer[1..=2]) as f32;
+                rotation.z = to_i16(&buffer[9..=10]) as f32;
             }
 
             (translation, rotation)
@@ -128,11 +134,16 @@ struct SpaceMousePlugin {
     type_label: Option<Gd<Label>>,
     translation_label: Option<Gd<Label>>,
     rotation_label: Option<Gd<Label>>,
+
+    // 3d
+    camera: Option<Gd<Camera3D>>,
 }
 
 #[godot_api]
 impl IEditorPlugin for SpaceMousePlugin {
     fn ready(&mut self) {
+        // self.to_gd().get_viewport().unwrap().print_tree_pretty();
+        self.camera = self.to_gd().get_viewport().unwrap().get_camera_3d();
 
         let hidapi = HidApi::new().unwrap();
         if let Some(spacemouse) = SpaceMouse::find(&hidapi) {
@@ -149,9 +160,29 @@ impl IEditorPlugin for SpaceMousePlugin {
             type_label.set_text(&spacemouse.format.to_string());
         }
     }
+
+    fn handles(&self, _o: Gd<Object>) -> bool {
+        true
     }
 
-    fn physics_process(&mut self, _delta: f64) {
+    fn forward_3d_gui_input(
+        &mut self,
+        camera: Option<Gd<Camera3D>>,
+        _event: Option<Gd<InputEvent>>,
+    ) -> i32 {
+        if camera.is_some()
+            && self.camera.as_ref().is_none_or(|current| {
+                current.get_camera_rid() != camera.as_ref().unwrap().get_camera_rid()
+            })
+        {
+            self.camera = camera;
+            print(&["Set camera".to_variant()]);
+        }
+
+        0
+    }
+
+    fn physics_process(&mut self, delta: f64) {
         if self.hid_device.is_none() {
             return;
         }
@@ -171,9 +202,9 @@ impl IEditorPlugin for SpaceMousePlugin {
                 .set_text(&rotation.to_string());
 
             if let Some(camera) = self.camera.as_mut() {
-                camera.translate(translation);
-            } else {
-                self.camera = self.to_gd().get_viewport().unwrap().get_camera_3d();
+                camera.translate(translation * 0.25 * delta as f32);
+                let new_rotation = camera.get_rotation() + (rotation * 0.05 * delta as f32);
+                camera.set_rotation(new_rotation);
             }
         }
     }
@@ -203,5 +234,19 @@ impl IEditorPlugin for SpaceMousePlugin {
         if let Some(control) = self.control.as_ref() {
             self.to_gd().remove_control_from_docks(control);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing_int16() {
+        let buffer: &[u8] = &[1, 0x00, 0x10, 0xff, 0x00, 0xff, 0xff];
+
+        assert_eq!(to_i16(&buffer[1..=2]), 4096);
+        assert_eq!(to_i16(&buffer[3..=4]), 255);
+        assert_eq!(to_i16(&buffer[5..=6]), -1);
     }
 }
